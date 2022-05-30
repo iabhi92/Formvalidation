d653d4488bc7b9e2135975c05aab314b48837b70
Abhinav kumar <pinku2c@gmail.com>
2022-05-30 11:51:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*var nameerror=document.getElementById("NAME_error");
+var projecterror=document.getElementById(PROJ_error);
+var bucketerror=document.getElementById(BUCK_error);
+var inputerror=document.getElementById(Storage_error);
+var crederror=document.getElementById(CRED_error);
+
+ 
+
+function validateName(){
+    var name=getElementById('pipeline_name').value;
+
+    if(name.length<5){
+        nameerror.innerHTML='NAME IS REQUIRED';
+    }
+}*/
+
+console.log('abhinav')
